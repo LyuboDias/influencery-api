c4a7fb32ada7df0f7b2392fbb512309e6f327d57
Lyubomir Dias <lyubomirdias@gmail.com>
2022-03-06 21:21:45 +0000

    fixing primary_tag

--- db/migrate/20220305165800_add_primary_tag_to_influencers.rb
+++
@@ -1,5 +0,0 @@
-class AddPrimaryTagToInfluencers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :influencers, :primary_tag, :bigint, null: :false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_05_172527) do
+ActiveRecord::Schema.define(version: 2022_03_06_212029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2022_03_05_172527) do
     t.string "profile_pic_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "primary_tag_id", null: false
     t.index ["platform_id"], name: "index_influencers_on_platform_id"
   end
 
